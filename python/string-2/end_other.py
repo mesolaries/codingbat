cf19828e6b1fc08abc5f4d67b839b3372aaed9d8
mesolaries <mnf.emil@gmail.com>
2017-08-08 23:35:02 +0400

    Solved end_other.py

---
+++ python/string-2/end_other.py
@@ -0,0 +1,13 @@
+''' Given two strings, return True if either of the strings appears at the very end of the other string, ignoring upper/lower case differences (in other words, the computation should not be "case sensitive"). Note: s.lower() returns the lowercase version of a string. '''
+
+# end_other('Hiabc', 'abc') → True
+# end_other('AbC', 'HiaBc') → True
+# end_other('abc', 'abXabc') → True
+
+def end_other(a, b):
+  a = a.lower()
+  b = b.lower()
+  if len(a) > len(b):
+    return b in a[-len(b):]
+  else:
+    return a in b[-len(a):]
