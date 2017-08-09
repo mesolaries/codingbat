9017ecfdd301faaea52e37b6e1fe7e2ece464369
Emil Manafov <mnf.emil@gmail.com>
2017-08-09 16:34:50 +0400

    Solved sum67.py

---
+++ python/list-2/sum67.py
@@ -0,0 +1,12 @@
+''' Return the sum of the numbers in the array, except ignore sections of numbers starting with a 6 and extending to the next 7 (every 6 will be followed by at least one 7). Return 0 for no numbers. '''
+
+# sum67([1, 2, 2]) → 5
+# sum67([1, 2, 2, 6, 99, 99, 7]) → 5
+# sum67([1, 1, 6, 7, 2]) → 4
+
+def sum67(nums):
+  while 6 in nums:
+    i=nums.index(6)
+    k=nums.index(7,i)
+    del nums[i:k+1]
+  return sum(nums)
