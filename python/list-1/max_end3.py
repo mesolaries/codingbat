5696cce43ad8c51c75f3e329cf9230db99a1ead7
Emil Manafov <mnf.emil@gmail.com>
2017-08-11 13:57:59 +0400

    Solved max_end3.py

---
+++ python/list-1/max_end3.py
@@ -0,0 +1,9 @@
+''' Given an array of ints length 3, figure out which is larger, the first or last element in the array, and set all the other elements to be that value. Return the changed array. '''
+
+# max_end3([1, 2, 3]) → [3, 3, 3]
+# max_end3([11, 5, 9]) → [11, 11, 11]
+# max_end3([2, 11, 3]) → [3, 3, 3]
+
+def max_end3(nums):
+  nums = [max(nums[0], nums[-1])] * 3
+  return nums
